3c39bb422c85be888e332d5e3d3ac2e8cec12d31
John Vee Uminga <johnveeuminga@example.com>
2019-05-23 02:09:03 +0800

    General changes.

--- src/components/NewsSection.vue
+++ src/components/NewsSection.vue
@@ -15,7 +15,7 @@
           <div class="news__col">
             <img class='news__img' src="@/assets/democracy.png" alt="">
             <h3 class='text-center'>Democracy</h3>
-            <p>We focus less on domestic politics and more on the key aspects of democracies such as free and fair elections, the rule of law, political freedom, an independent judiciary and a healthy, vibrant press. </p>
+            <p>We focus less on domestic politics and more on the key aspects of democracies such as free and fair elections, the rule of law, political freedom, an independent judiciary and a healthy, vibrant press.</p>
           </div>
         </div>
         <div class="col-md-4">
@@ -29,14 +29,14 @@
           <div class="news__col">
             <img class='news__img' src="@/assets/human-rights.png" alt="">
             <h3 class='text-center'>Human Rights</h3>
-            <p>Nonprofits play a major role in helping people and communities. We report on voluntarism, philanthropy, nonprofit trends, innovative ideas and government policies that affect nonprofits.</p>
+            <p>We bring you stories of people fighting for freedom of speech, assembly and movement, freedom from torture, slavery and arbitrary arrest, and the rights to a fair trial, asylum and personal security.</p>
           </div>
         </div>
         <div class="col-md-4">
           <div class="news__col">
             <img class='news__img' src="@/assets/science-and-tech.png" alt="">
             <h3 class='text-center'>Science and Tech</h3>
-            <p>We bring you stories of people fighting for freedom of speech, assembly and movement, freedom from torture, slavery and arbitrary arrest, and the rights to a fair trial, asylum and personal security.</p>
+            <p>We tell you about key advances in health and medicine, new discoveries in science, and emerging changes in technology that improve communication, productivity and quality of life.</p>
           </div>
         </div>
         <div class="col-md-4">
